7079590a4e4ce7ec7a6bf683dd185cecc3024ebe
Bakytaman31 <bakytaman228337@gmail.com>
2022-02-19 14:50:08 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
+    requires javafx.graphics;
+
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+    exports com.example.demo.Controller;
+    opens com.example.demo.Controller to javafx.fxml;
+}
